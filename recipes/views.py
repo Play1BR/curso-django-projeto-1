1c40b242688c4494502b53eed29185c76e3162ee
Victor Carvalho <dev.victorcarvalho@gmail.com>
2026-07-13 20:46:43 -0300

    feat: setup inicial do projeto Django e app recipes

---
+++ recipes/views.py
@@ -0,0 +1,16 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def home(request):
+    return render(request, 'recipes/home.html', context={
+        'name': 'Victor Carvalho',
+    })
+
+
+def sobre(request):
+    return HttpResponse('SOBRE')
+
+
+def contato(request):
+    return HttpResponse('CONTATO')
