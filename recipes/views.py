443c07f963297052d8f62fe70c40c58531f4706b
Victor Carvalho <dev.victorcarvalho@gmail.com>
2026-07-14 20:54:51 -0300

    refactor: remove funcoes temporarias (contato e sobre)

--- recipes/views.py
+++ recipes/views.py
@@ -1,4 +1,3 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 
@@ -7,10 +6,3 @@ def home(request):
         'name': 'Victor Carvalho',
     })
 
-
-def sobre(request):
-    return HttpResponse('SOBRE')
-
-
-def contato(request):
-    return HttpResponse('CONTATO')
