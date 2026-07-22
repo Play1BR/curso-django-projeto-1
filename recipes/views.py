57e12cd730e1975b632dc7d2362427cb548dd67f
Victor Carvalho <dev.victorcarvalho@gmail.com>
2026-07-22 20:43:22 -0300

    feat: Injeta dados gerados automaticamente

--- recipes/views.py
+++ recipes/views.py
@@ -1,13 +1,18 @@
+from random import randint
+
 from django.shortcuts import render
+
+from utils.recipes.factory import make_recipe
 
 
 def home(request):
     return render(request, 'recipes/pages/home.html', context={
-        'name': 'Victor Carvalho',
+
+        'recipes': [make_recipe() for _ in range(9)]
     })
 
 
 def recipe(request, id):
     return render(request, 'recipes/pages/recipe_view.html', context={
-        'name': 'Victor Carvalho',
+        'recipe': make_recipe()
     })
